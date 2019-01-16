21647d7779eeb426ed3875a357f5647b35d606a2
Zulhilmi Ghiffary <zulhilmighifary218@gmail.com>
2019-01-16 19:36:33 +0700

    update limit

--- app/Http/Controllers/Web/TopTrackController.php
+++ app/Http/Controllers/Web/TopTrackController.php
@@ -10,10 +10,12 @@ class TopTrackController extends Controller
 {
     public function index()
     {
+        $songs = SongHelper::$limit = 15;
         $songs = SongHelper::$orderKey   = 'counter';
         $songs = SongHelper::$order      = 'desc';
         $songs = SongHelper::getSong();
 
+        $newest = SongHelper::$limit = 15;
         $newest = SongHelper::$orderKey   = 'o_id';
         $newest = SongHelper::$order      = 'desc';
         $newest = SongHelper::getSong();
